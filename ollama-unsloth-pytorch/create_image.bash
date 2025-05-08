b7ca63e844ba6ca76e17d9e6e1942eb069209836
hcstubbe <48794922+hcstubbe@users.noreply.github.com>
2025-05-08 15:19:25 +0200

    update

--- ollama-unsloth-pytorch/create_image.bash
+++ ollama-unsloth-pytorch/create_image.bash
@@ -1,2 +1,5 @@
 VERSION=$1
-docker build --no-cache -t hstubbe/ollama_unsloth:pytorch2.5.0-cuda12.4-cudnn9-devel-$VERSION -f Dockerfile .
+docker build --no-cache -t hstubbe/ollama_unsloth:pytorch2.5.0-cuda12.4-cudnn9-devel-$VERSION -f Dockerfile . && \
+docker tag hstubbe/ollama_unsloth:pytorch2.5.0-cuda12.4-cudnn9-devel-$VERSION hstubbe/ollama_unsloth:pytorch2.5.0-cuda12.4-cudnn9-devel-latest && \
+docker push hstubbe/ollama_unsloth:pytorch2.5.0-cuda12.4-cudnn9-devel-$VERSION && \
+docker push hstubbe/ollama_unsloth:pytorch2.5.0-cuda12.4-cudnn9-devel-latest
