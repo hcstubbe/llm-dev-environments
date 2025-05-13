18c780e62930478cd5e2489f979ad3d65e48738f
hcstubbe <48794922+hcstubbe@users.noreply.github.com>
2025-05-13 15:53:17 +0200

    Update create_image.bash

--- ollama-unsloth-pytorch/create_image.bash
+++ ollama-unsloth-pytorch/create_image.bash
@@ -1,10 +1,11 @@
 VERSION=$1
-OPTIONS=$2
+OWNER=$2
+OPTIONS=$3
 if [ -z "$VERSION" ]; then
   echo "Usage: $0 <version> [options]"
   exit 1
 fi
 docker build $OPTIONS -t hstubbe/ollama_unsloth:pytorch2.5.0-cuda12.4-cudnn9-devel-$VERSION -f Dockerfile . && \
-docker tag hstubbe/ollama_unsloth:pytorch2.5.0-cuda12.4-cudnn9-devel-$VERSION hstubbe/ollama_unsloth:pytorch2.5.0-cuda12.4-cudnn9-devel-latest && \
-docker push hstubbe/ollama_unsloth:pytorch2.5.0-cuda12.4-cudnn9-devel-$VERSION && \
-docker push hstubbe/ollama_unsloth:pytorch2.5.0-cuda12.4-cudnn9-devel-latest
+docker tag $OWNER/ollama_unsloth:pytorch2.5.0-cuda12.4-cudnn9-devel-$VERSION hstubbe/ollama_unsloth:pytorch2.5.0-cuda12.4-cudnn9-devel-latest && \
+docker push $OWNER/ollama_unsloth:pytorch2.5.0-cuda12.4-cudnn9-devel-$VERSION && \
+docker push $OWNER/ollama_unsloth:pytorch2.5.0-cuda12.4-cudnn9-devel-latest
